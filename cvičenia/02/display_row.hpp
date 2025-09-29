4d46d161e1c3e460d13cce4678c638808bba54c9
david <89749147+davight@users.noreply.github.com>
2025-09-29 11:48:15 +0200

    innit

---
+++ cvičenia/02/display_row.hpp
@@ -0,0 +1,17 @@
+#ifndef ASCIID_DISPLAY_ROW_HPP
+#define ASCIID_DISPLAY_ROW_HPP
+
+namespace asciid
+{
+
+    class DisplayRow
+    {
+        public:
+            DisplayRow();
+            int getDisplayCount() const;
+
+    };
+
+};
+
+#endif
